cf7edc72aa8778de80287e193eb0d2c0288d4e9d
Rakshit <rakshiit49@gmail.com>
2026-06-29 22:04:44 +0530

    Time: 0 ms (100%), Space: 13.6 MB (77.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,7 +4,6 @@ public:
         for(int i=0;i<nums.size();i++){
             if(target<=nums[i]){
                 return i;
-                break;
             }
         }
         return nums.size();
